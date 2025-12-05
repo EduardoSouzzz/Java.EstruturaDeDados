85e586bd7f23e76f938f261392d3505944daef6a
Eduardo Souza <dudujv1910@gmail.com>
2025-12-05 19:22:55 -0300

    Add Score class to manage player scores

---
+++ Vetores/Ranking/Score.java
@@ -0,0 +1,26 @@
+public class Score {
+
+    private String jogador;
+    private int pontos;
+
+    public Score(String jogador, int pontos) {
+        this.jogador = jogador;
+        this.pontos = pontos;
+    }
+
+    public String getJogador() {
+        return jogador;
+    }
+
+    public void setJogador(String jogador) {
+        this.jogador = jogador;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
+}
